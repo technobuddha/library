fd3e57fab177f5e1469a437dc9f7cbc32a538606
Phil Hill <phil@technobuddha.com>
2021-02-01 09:26:37 -0500

    Fix typing in dataURL

--- src/dataURL.ts
+++ src/dataURL.ts
@@ -1,7 +1,18 @@
 import map   from 'lodash/map';
 import build from './build';
 
-export type BinaryObject = ArrayBuffer | DataView | Int8Array | Uint8Array | Uint8ClampedArray | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array;
+export type BinaryObject =
+    ArrayBuffer |
+    DataView |
+    Int8Array |
+    Uint8Array |
+    Uint8ClampedArray |
+    Int16Array |
+    Uint16Array |
+    Int32Array |
+    Uint32Array |
+    Float32Array |
+    Float64Array;
 
 /**
  * Convert any binary object into a data URL
@@ -11,12 +22,14 @@ export type BinaryObject = ArrayBuffer | DataView | Int8Array | Uint8Array | Uin
  * @returns The data URL
  */
 export function dataURL(input: BinaryObject, mimeType: string):  string {
+    const buffer = input instanceof ArrayBuffer ? input : input.buffer;
+
     if(typeof btoa !== 'undefined') {
-        const bytes = new Uint8Array(input instanceof ArrayBuffer ? input : input.buffer);
+        const bytes = new Uint8Array(buffer);
 
         return `data:${mimeType};base64,${btoa(build(map(bytes, c => String.fromCharCode(c))))}`;
     }
-    return `data:${mimeType};base64,${Buffer.from(input).toString('base64')}`;
+    return `data:${mimeType};base64,${Buffer.from(buffer).toString('base64')}`;
 }
 
 export default dataURL;
