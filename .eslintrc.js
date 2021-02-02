3fd76978d8b79fef68c2c736d43644483ece602b
Phil Hill <phil@technobuddha.com>
2021-02-02 08:48:36 -0500

    Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     'plugins': [
         '@typescript-eslint',
-        //'eslint-plugin-tsdoc',
+        'eslint-plugin-tsdoc',
         'chai-expect',
     ],
     'extends': [
