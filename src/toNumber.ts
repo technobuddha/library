c0e849ae81982b8bfb5c807b353eefa944bbadb7
Phil Hill <phil@technobuddha.com>
2021-04-06 15:22:04 -0400

    support boolean datatypes

--- src/toNumber.ts
+++ src/toNumber.ts
@@ -1,8 +1,9 @@
-import isString from 'lodash/isString';
-import isNumber from 'lodash/isNumber';
+import isNumber  from 'lodash/isNumber';
+import isBoolean from 'lodash/isBoolean';
+import isString  from 'lodash/isString';
 
 export function toNumber(entity: unknown): number {
-    return isNumber(entity) ? entity : isString(entity) ? parseFloat(entity) : NaN;
+    return isNumber(entity) ? entity : isBoolean(entity) ? (entity ? 1 : 0) : isString(entity) ? parseFloat(entity) : NaN;
 }
 
 export default toNumber;
