7c70157a729dca64dabc64d8578242e5d2f0e08f
Phil Hill <phil@technobuddha.com>
2021-07-22 16:59:19 -0400

    Use same lint rules on all projects

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-require-imports */
 const path = require('path');
 const base = require('../../.eslintrc.base');
 
